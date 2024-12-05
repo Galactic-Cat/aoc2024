a39cfc60101e3ed762aa6336fbb0cc05306d2c64
Galactic-Cat <11347559+Galactic-Cat@users.noreply.github.com>
2024-12-05 23:28:51 +0100

    Setup template for my mac and created files of day 6

--- day05/src/Solution.hs
+++ day05/src/Solution.hs
@@ -1,52 +1,30 @@
 module Solution (solve, solve2, Rule (Rule)) where
-  import Prelude hiding (traverse)
+  import Prelude hiding (filter)
+  import Data.List (elemIndex)
 
   data Rule = Rule Int Int
     deriving Show
 
-  inplace :: Int -> [Rule] -> [Int] -> [Int] -> Bool
-  inplace x rs ps ns = validateRules (ps ++ [x] ++ ns) rs
+  filter :: [[Int]] -> Rule -> [[Int]]
+  filter []     _ = []
+  filter (x:xs) r
+    | validateRule x r = x : filter xs r
+    | otherwise        = filter xs r
+
+  filters :: [[Int]] -> [Rule] -> [[Int]]
+  filters = foldl filter
 
   middle :: [a] -> a
   middle xs = xs !! (length xs `div` 2)
 
-  place :: Int -> [Rule] -> [Int] -> [Int]
-  place x rs = place' []
-    where
-      place' ps ns
-        | inplace x rs ps ns = x : ns
-        | otherwise          = case ns of
-                                 []      -> error "Failed to place"
-                                 (n:nss) -> n : place' (ps ++ [n]) nss
-
-  placeAll :: [Int] -> [Int] -> [Rule] -> [Int]
-  placeAll xs a rs = foldl (\a' x -> place x rs a') a xs
-
   solve :: ([Rule], [[Int]]) -> Int
-  solve (_ , [])   = 0
-  solve (rs, x:xs)
-    | validateRules x rs = middle x + solve (rs, xs)
-    | otherwise          = solve (rs, xs)
+  solve (rs, xs) = sum (map middle (filters xs rs))
 
   solve2 :: ([Rule], [[Int]]) -> Int
-  solve2 ( _, [])   = 0
-  solve2 (rs, x:xs)
-    | validateRules x rs = solve (rs, xs)
-    | otherwise          = middle (placeAll x [] rs) + solve (rs, xs)
+  solve2 _ = 3
 
-  traverse :: ([Int] -> Int -> [Int] -> a) -> [Int] -> [a]
-  traverse _ []     = []
-  traverse f (x:xs) = traverse' [] x xs
-    where
-      traverse' ps c []     = [f ps c []]
-      traverse' ps c (n:ns) = f ps c (n:ns) : traverse' (c:ps) n ns
-
-  validateRule :: Rule -> [Int] -> Int -> [Int] -> Bool
-  validateRule (Rule a b) pss c nss
-    | c == a    = b `notElem` pss
-    | c == b    = a `notElem` nss
-    | otherwise = True
-
-  validateRules :: [Int] -> [Rule] -> Bool
-  validateRules _  []     = True
-  validateRules xs (r:rs) = and (traverse (validateRule r) xs) && validateRules xs rs
+  validateRule :: [Int] -> Rule -> Bool
+  validateRule xs (Rule a b) =
+    case (a `elemIndex` xs, b `elemIndex` xs) of
+      (Just ia, Just ib) -> ia < ib
+      _                  -> True
