d59a73ea2ce8091a83a107259a458f8a6a8f6f0d
Simon van Hus <s67549@eon.com>
2024-12-06 09:49:16 +0100

    Working on day 6

--- day06/src/Parse.hs
+++ day06/src/Parse.hs
@@ -1,6 +1,44 @@
 module Parse (parse) where
-  parse :: String -> IO [String]
+  import Solution (
+    Coordinate,
+    Direction (North, East, South, West),
+    Guard (Guard),
+    Tile (Unvisited, Visited, Obstructed))
+
+  best :: Maybe a -> Maybe a -> Maybe a
+  best (Just x) _ = Just x
+  best _ (Just y) = Just y
+  best _        _ = Nothing
+
+  parse :: String -> IO ([[Tile]], Guard)
   parse path =
     do
       contents <- readFile path
-      return . words $ contents
+      return $ parseWorld (lines contents)
+
+  parseLine :: String -> Coordinate -> ([Tile], Maybe Guard)
+  parseLine []     _        = ([], Nothing)
+  parseLine (x:xs) (cx, cy) =
+    let (ws, gs) = parseLine xs (cx + 1, cy)
+    in  case x of
+        '.' -> (Unvisited : ws, gs)
+        '#' -> (Obstructed : ws, gs)
+        '^' -> (Visited : ws, Just $ Guard (cx, cy) North)
+        '>' -> (Visited : ws, Just $ Guard (cx, cy) East)
+        'v' -> (Visited : ws, Just $ Guard (cx, cy) South)
+        '<' -> (Visited : ws, Just $ Guard (cx, cy) West)
+        _   -> error $ "Unrecogonized character '" ++ [x] ++ "'"
+
+  parseWorld :: [String] -> ([[Tile]], Guard)
+  parseWorld s = 
+    let (ls, g) = lined (length s - 1)
+    in  (ls, assert g)
+    where
+      assert (Just x) = x
+      assert Nothing  = error "Guard not found"
+
+      lined 0 = let (l, g) = parseLine (head s) (0, 0) in ([l], g)
+      lined y =
+        let (ls, gs) = lined (y - 1)
+        in  let (l,  g) = parseLine (s !! y) (0, y)
+            in  (l : ls, best g gs)
--- day06/src/Solution.hs
+++ day06/src/Solution.hs
@@ -1,3 +1,29 @@
-module Solution (solve) where
-  solve :: IO ()
+module Solution (
+  solve,
+  Coordinate,
+  Direction (North, East, South, West),
+  Guard (Guard),
+  Tile (Unvisited, Visited, Obstructed),
+) where
+
+  type Coordinate = (Int, Int)
+  data Direction = North | East | South | West
+  data Guard = Guard Coordinate Direction
+  data Tile = Unvisited | Visited | Obstructed
+
+  getPos :: Coordinate -> Direction -> Coordinate
+  getPos (x, y) North = (x - 1, y)
+  getPos (x, y) East  = (x,     y + 1)
+  getPos (x, y) South = (x + 1, y)
+  getPos (x, y) West  = (x,     y - 1)
+
+  getTile :: [[Tile]] -> Coordinate -> Maybe Tile
+  getTile tg (x, y) =
+    if   x >= 0 && y >= 0 && x < length (head tg) && y < length tg
+    then Just $ (tg !! y) !! x
+    else Nothing
+
+  iterate :: [[Tile]] -> Guard -> ([[Tile]], Maybe Guard)
+
+  solve :: ([[Tile]], Guard) -> IO ()
   solve = print "No solution"
