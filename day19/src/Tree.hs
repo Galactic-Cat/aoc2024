82de001f9a00c106c60aa7f22ee2f44cb81c9eb2
Simon van Hus <s67549@eon.com>
2024-12-19 16:05:23 +0100

    Working on new solution for day 19

--- day19/src/Tree.hs
+++ day19/src/Tree.hs
@@ -1,47 +1,87 @@
-module Tree (Pattern, PatternTree, add, empty) where
+module Tree (Pattern, PatternTree, add, matchFull, empty) where
   import Data.Maybe (fromMaybe)
 
-  data Pattern = White PatternTree | Blue PatternTree | Black PatternTree | Red PatternTree | Green PatternTree
-  type PatternTree = (Maybe Pattern, Maybe Pattern, Maybe Pattern, Maybe Pattern, Maybe Pattern)
+  data Pattern = Pattern Char PatternTree
+  type PatternTree = (Maybe Pattern, Maybe Pattern, Maybe Pattern, Maybe Pattern, Maybe Pattern, Bool)
 
   add :: String -> PatternTree -> PatternTree
-  add []       t = t
-  add ('w':cs) t = (Just (inSubTree (getSafe t 0) (add cs)), getU t, getB t, getR t, getG t)
-  add ('u':cs) t = (getW t, Just (inSubTree (getSafe t 1) (add cs)), getB t, getR t, getG t)
-  add ('b':cs) t = (getW t, getU t, Just (inSubTree (getSafe t 2) (add cs)), getR t, getG t)
-  add ('r':cs) t = (getW t, getU t, getB t, Just (inSubTree (getSafe t 3) (add cs)), getG t)
-  add ('g':cs) t = (getW t, getU t, getB t, getR t, Just (inSubTree (getSafe t 4) (add cs)))
+  add []       t = (getW t, getU t, getB t, getR t, getG t, True)
+  add ('w':cs) t = (Just (inSubTree (getSafe t 0) (add cs)), getU t, getB t, getR t, getG t, getEnd t)
+  add ('u':cs) t = (getW t, Just (inSubTree (getSafe t 1) (add cs)), getB t, getR t, getG t, getEnd t)
+  add ('b':cs) t = (getW t, getU t, Just (inSubTree (getSafe t 2) (add cs)), getR t, getG t, getEnd t)
+  add ('r':cs) t = (getW t, getU t, getB t, Just (inSubTree (getSafe t 3) (add cs)), getG t, getEnd t)
+  add ('g':cs) t = (getW t, getU t, getB t, getR t, Just (inSubTree (getSafe t 4) (add cs)), getEnd t)
   add (_  :_ ) _ = error "Pattern character not recognized in add"
 
+  charToInt :: Char -> Int
+  charToInt 'w' = 0
+  charToInt 'u' = 1
+  charToInt 'b' = 2
+  charToInt 'r' = 3
+  charToInt 'g' = 4
+  charToInt _   = error "charToInt char not recognized"
+
   empty :: PatternTree
-  empty = (Nothing, Nothing, Nothing, Nothing, Nothing)
+  empty = (Nothing, Nothing, Nothing, Nothing, Nothing, False)
 
-  getB :: (a, b, c, d, e) -> c
-  getB (_, _, b, _, _) = b
+  get :: PatternTree -> Int -> Maybe Pattern
+  get (w, _, _, _, _, _) 0 = w
+  get (_, u, _, _, _, _) 1 = u
+  get (_, _, b, _, _, _) 2 = b
+  get (_, _, _, r, _, _) 3 = r
+  get (_, _, _, _, g, _) 4 = g
+  get _                  _ = error "get int out of range"
 
-  getG :: (a, b, c, d, e) -> e
-  getG (_, _, _, _, g) = g
-  
-  getR :: (a, b, c, d, e) -> d
-  getR (_, _, _, r, _) = r
+  getB :: (a, b, c, d, e, f) -> c
+  getB (_, _, b, _, _, _) = b
 
-  getSafe :: (Maybe Pattern, Maybe Pattern, Maybe Pattern, Maybe Pattern, Maybe Pattern) -> Int -> Pattern
-  getSafe (w, _, _, _, _) 0 = fromMaybe (White empty) w
-  getSafe (_, u, _, _, _) 1 = fromMaybe (White empty) u
-  getSafe (_, _, b, _, _) 2 = fromMaybe (White empty) b
-  getSafe (_, _, _, r, _) 3 = fromMaybe (White empty) r
-  getSafe (_, _, _, _, g) 4 = fromMaybe (White empty) g
-  getSafe _               _ = error "getSafe int out of range"
+  getEnd :: (a, b, c, d, e, f) -> f
+  getEnd (_, _, _, _, _, e) = e
 
-  getU :: (a, b, c, d, e) -> b
-  getU (_, u, _, _, _) = u
-  
-  getW :: (a, b, c, d, e) -> a
-  getW (w, _, _, _, _) = w
-  
+  getG :: (a, b, c, d, e, f) -> e
+  getG (_, _, _, _, g, _) = g
+
+  getR :: (a, b, c, d, e, f) -> d
+  getR (_, _, _, r, _, _) = r
+
+  getSafe :: PatternTree -> Int -> Pattern
+  getSafe (w, _, _, _, _, _) 0 = fromMaybe (Pattern 'w' empty) w
+  getSafe (_, u, _, _, _, _) 1 = fromMaybe (Pattern 'u' empty) u
+  getSafe (_, _, b, _, _, _) 2 = fromMaybe (Pattern 'b' empty) b
+  getSafe (_, _, _, r, _, _) 3 = fromMaybe (Pattern 'r' empty) r
+  getSafe (_, _, _, _, g, _) 4 = fromMaybe (Pattern 'g' empty) g
+  getSafe _                  _ = error "getSafe int out of range"
+
+  getU :: (a, b, c, d, e, f) -> b
+  getU (_, u, _, _, _, _) = u
+
+  getW :: (a, b, c, d, e, f) -> a
+  getW (w, _, _, _, _, _) = w
+
   inSubTree :: Pattern -> (PatternTree -> PatternTree) -> Pattern
-  inSubTree (White t) f = White $ f t
-  inSubTree (Blue  t) f = Blue  $ f t
-  inSubTree (Black t) f = Black $ f t
-  inSubTree (Red   t) f = Red   $ f t
-  inSubTree (Green t) f = Green $ f t
+  inSubTree (Pattern c t) f = Pattern c $ f t
+
+  isEmpty :: PatternTree -> Bool
+  isEmpty (Nothing, Nothing, Nothing, Nothing, Nothing, _) = True
+  isEmpty _                                                = False
+
+  matchFull :: String -> PatternTree -> Bool
+  matchFull []     t = getEnd t
+  matchFull (c:cs) t =
+    case get t (charToInt c) of
+      Just (Pattern _ pt) -> matchFull cs pt
+      Nothing             -> False
+
+  match :: String -> PatternTree -> [(String, String)]
+  match = match' []
+    where
+      match' :: String -> String -> PatternTree -> [(String, String)]
+      match' a []     t = [(a, []) | getEnd t]
+      match' a (c:cs) t = _
+
+        where
+          ms =
+            if   getEnd t
+            then (a ++ [c], cs)
+            else
+        
